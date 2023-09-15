c6686cff2d2a0dc66b8b793b312c2c9e3825a6cd
Marios Sofokleous <msofokleous@noteris.com>
2023-09-15 17:52:38 +0300

    Setting up Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,8 @@ module.exports = {
       },
     },
 
+    `gatsby-plugin-sass`,
+
     /**
      * The following two plugins are required if you want to use Gatsby image
      * See https://www.gatsbyjs.com/docs/gatsby-image/#setting-up-gatsby-image
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import parse from "html-react-parser"
+import "./layout.scss"
 
 const Layout = ({ isHomePage, children }) => {
   const {
